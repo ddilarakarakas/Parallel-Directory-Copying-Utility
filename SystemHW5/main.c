dfd9da82f46f089bdc9eb744ab1ac1f4d25682ec
ddilarakarakas <karakasdilara1@gmail.com>
2023-06-02 18:39:54 +0300

    Initial Commit

---
+++ SystemHW5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SystemHW5
+//
+//  Created by Dilara Karakaş on 2.06.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
